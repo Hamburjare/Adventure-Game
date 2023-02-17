a2ac1cf28a7c2826258cc4ae831b2ff860f3c82d
Hamburjare <93974508+Hamburjare@users.noreply.github.com>
2023-02-17 10:13:26 +0200

    bug fixes

--- Assets/Scripts/Player/SwordController.cs
+++ Assets/Scripts/Player/SwordController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class SwordController : MonoBehaviour
 {
-    [SerializeField] int m_damageAmount = 1;
-    
-    private void OnCollisionEnter(Collision other) {
-        if (other.gameObject.CompareTag("Enemy")) {
+    [SerializeField]
+    int m_damageAmount = 1;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
             // other.gameObject.GetComponent<EnemyHealth>().TakeDamage(damageAmount_);
             AudioManager.instance.Play("SwordHit");
+            
+            if (GameManager.s_isSpiderDead)
+                return;
+
             GameManager.Instance.RemoveSpiderHealth(m_damageAmount);
             GameManager.Instance.AddXp(m_damageAmount);
         }
